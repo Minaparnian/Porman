1a564da4eb375f67e88c39332f0ce143e9d298af
Mina <minap@equitise.com>
2019-04-08 17:15:06 +1000

    create project component

--- src/App.js
+++ src/App.js
@@ -5,7 +5,8 @@ import { Route } from 'react-router';
 import Dashboard from './components/dashboard/Dashboard';
 import ProjectDetails from './components/projects/ProjectDetails';
 import SignIn from './components/auth/SignIn';
-import SignUp from './components/auth/SignUp'
+import SignUp from './components/auth/SignUp';
+import CreateProject from './components/projects/CreateProject'
 
 class App extends Component {
   render() {
@@ -17,7 +18,8 @@ class App extends Component {
             <Route path='/' exact component={Dashboard}></Route>
             <Route path='/project/:id' component={ProjectDetails}></Route>
             <Route path='/signin' component={SignIn}></Route>
-            <Route path='/signUp' component={SignUp}></Route>
+            <Route path='/signup' component={SignUp}></Route>
+            <Route path='/createproject' component={CreateProject}></Route>
         </Switch>
         </div>
       </BrowserRouter>
