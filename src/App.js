0ec8dfc773a866b83077ce58f214c9cd9c13267e
Mina <minap@equitise.com>
2019-04-08 16:55:22 +1000

    make sign up component and add Route path to link in the component

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import { BrowserRouter, Switch } from 'react-router-dom';
 import { Route } from 'react-router';
 import Dashboard from './components/dashboard/Dashboard';
 import ProjectDetails from './components/projects/ProjectDetails';
-import SignIn from './components/auth/SignIn'
+import SignIn from './components/auth/SignIn';
+import SignUp from './components/auth/SignUp'
 
 class App extends Component {
   render() {
@@ -16,6 +17,7 @@ class App extends Component {
             <Route path='/' exact component={Dashboard}></Route>
             <Route path='/project/:id' component={ProjectDetails}></Route>
             <Route path='/signin' component={SignIn}></Route>
+            <Route path='/signUp' component={SignUp}></Route>
         </Switch>
         </div>
       </BrowserRouter>
